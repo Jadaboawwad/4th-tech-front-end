a8c6b0444f4fe6a0d621847babb573cbf65cd5c5
Jadaboawwad <jadaboawwad@gmail.com>
2024-12-12 17:26:01 +0300

    feat : contact us added

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -3,14 +3,17 @@ import React from 'react'
 import Container from 'components/Atoms/Container/Container'
 import Header from 'components/Organisms/Header/Header'
 import ContactUsStyles from 'pages/ContactUs/ContactUs.module.css'
+import ContactUsHero from 'components/Organisms/ContactUsHero/ContactUsHero'
+import Footer from 'components/Organisms/Footer/Footer'
 
 const ContactUs = () => (
+  <Container className="appContainer">
+    <Header />
     <Container className={ContactUsStyles.contactUsWrapper}>
-      <Header />
-      <Container className={ContactUsStyles.contactUs}>
-        <></>
-      </Container>
+      <ContactUsHero />
     </Container>
-  )
+    <Footer />
+  </Container>
+)
 
 export default ContactUs
